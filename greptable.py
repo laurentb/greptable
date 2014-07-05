bf6725236fe09a58ee201c5e0fe1746c71ad0dab
Laurent Bachelier <laurent@bachelier.name>
2014-07-06 00:59:18 +0200

    Support for SQLite, make it easier to tap into some functions

--- greptable.py
+++ greptable.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals
 
+import sys
+
+from sqlalchemy import create_engine, inspect
+
 try:
     from urllib.parse import urlsplit
 except ImportError:
     from urlparse import urlsplit
-
-from sqlalchemy import create_engine, inspect
 
 
 class Server(object):
@@ -22,13 +24,16 @@ class Server(object):
     def __str__(self):
         return self.name
 
-    def databases(self):
+    def schemas(self):
         inspector = inspect(self.engine)
-        for dbname in inspector.get_schema_names():
-            yield Database(self, dbname)
+        dbs = inspector.get_schema_names()
+        if not len(dbs):
+            yield Schema(self, inspector.default_schema_name)
+        for dbname in dbs:
+            yield Schema(self, dbname)
 
 
-class Database(object):
+class Schema(object):
     def __init__(self, server, name):
         self.server = server
         self.name = name
@@ -39,30 +44,38 @@ class Database(object):
             yield Table(self, tablename)
 
     def __str__(self):
-        return '%s:%s' % (self.server, self.name)
+        return '%s:%s' % (self.server, self.name or '')
 
 
 class Table(object):
-    def __init__(self, database, name):
-        self.database = database
+    def __init__(self, schema, name):
+        self.schema = schema
         self.name = name
 
     def __str__(self):
-        return '%s:%s' % (self.database, self.name)
+        return '%s:%s' % (self.schema, self.name)
 
 
 def get_config(path='greptable.conf'):
     with open(path) as f:
         config = f.read()
+    return parse_config(config)
+
+
+def parse_config(config):
     for url in config.splitlines():
         yield Server(url)
 
 
+def print_servers(servers, outfile):
+    for server in servers:
+        print(server, file=outfile)
+        for schema in server.schemas():
+            print(schema, file=outfile)
+            for table in schema.tables():
+                print(table, file=outfile)
+
+
 if __name__ == '__main__':
     servers = get_config()
-    for server in servers:
-        print(server)
-        for database in server.databases():
-            print(database)
-            for table in database.tables():
-                print(table)
+    print_servers(servers, outfile=sys.stdout)
